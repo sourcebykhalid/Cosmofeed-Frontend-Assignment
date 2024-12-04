50e36075de29d00f551a73081710afb7c255b783
Khalid Shakeel <khalidshakeel46@gmail.com>
2024-12-04 13:26:30 +0530

    Modified components

--- src/utils/localStorage.js
+++
@@ -1,8 +0,0 @@
-export const saveTasksToLocalStorage = (tasks) => {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-};
-
-export const getTasksFromLocalStorage = () => {
-  const tasks = localStorage.getItem("tasks");
-  return tasks ? JSON.parse(tasks) : [];
-};
